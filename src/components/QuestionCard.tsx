eb0348c3e2ec820292e78209e72769062f9c9744
LHdensity <1252995152@qq.com>
2024-01-17 22:43:16 +0800

    添加QuestionCard组件props类型

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
 import styles from "./questionCard.module.css";
 
-const QuestionCard: FC = () => {
+type PropsType = {
+  _id: string;
+  title: string;
+  isStar: boolean;
+  isPublished: boolean;
+  answerCount: number;
+  createAt: string;
+};
+
+const QuestionCard: FC<PropsType> = (props: PropsType) => {
   return (
     <>
       <p>QuestionCard</p>
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -4,35 +4,35 @@ import styles from "./List.module.scss";
 
 const rawQuestionList = [
   {
-    id: "q1",
+    _id: "q1", // 为了和 mongoDB 保持一致，使用 _id
     title: "问卷1",
-    ispulished: false,
+    isPublished: false,
     isStar: false,
-    awserCoutn: 5,
+    answerCount: 5,
     createAt: "3月10日 13:43"
   },
   {
-    id: "q2",
+    _id: "q2",
     title: "问卷2",
-    ispulished: true,
+    isPublished: true,
     isStar: false,
-    awserCoutn: 5,
+    answerCount: 5,
     createAt: "5月20日 17:23"
   },
   {
-    id: "q3",
+    _id: "q3",
     title: "问卷3",
-    ispulished: false,
+    isPublished: false,
     isStar: true,
-    awserCoutn: 5,
+    answerCount: 5,
     createAt: "6月1日 13:53"
   },
   {
-    id: "q4",
+    _id: "q4",
     title: "问卷4",
-    ispulished: true,
+    isPublished: true,
     isStar: true,
-    awserCoutn: 5,
+    answerCount: 5,
     createAt: "7月70日 13:13"
   }
 ];
@@ -52,8 +52,8 @@ const List: FC = () => {
       </div>
       <div className={styles.content}>
         {questionList.map((q) => {
-          const { id } = q;
-          return <QuestionCard key={id} {...q} />;
+          const { _id } = q;
+          return <QuestionCard key={_id} {...q} />;
         })}
       </div>
       <div className={styles.footer}>footer</div>
