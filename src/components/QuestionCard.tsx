ec35c66e4a0821f05981c39b3da972746c8937f3
LHdensity <1252995152@qq.com>
2024-01-17 23:06:02 +0800

    修改questionCard样式，完善内部上下两栏

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -11,11 +11,35 @@ type PropsType = {
 };
 
 const QuestionCard: FC<PropsType> = (props: PropsType) => {
+  const { _id, title, createAt, answerCount, isPublished } = props;
+
   return (
     <div className={styles.container}>
-      <div>title</div>
-      button
-      <div></div>
+      <div className={styles.title}>
+        <div className={styles.left}>
+          <a href="#">{title}</a>
+        </div>
+        <div className={styles.right}>
+          {isPublished ? (
+            <span style={{ color: "green" }}>已发布</span>
+          ) : (
+            <span>未发布</span>
+          )}
+          <span>答卷:{answerCount}</span>
+          <span>{createAt}</span>
+        </div>
+      </div>
+      <div className={styles["button-container"]}>
+        <div className={styles.left}>
+          <button>编辑问卷</button>
+          <button>数据统计</button>
+        </div>
+        <div className={styles.right}>
+          <button>标星</button>
+          <button>复制</button>
+          <button>删除</button>
+        </div>
+      </div>
     </div>
   );
 };
