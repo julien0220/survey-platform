fee2678cbb578fd52de53807f018c9dcdc07275d
LHdensity <1252995152@qq.com>
2024-01-20 21:52:01 +0800

    使用antd的icon库，然后对manageLayout的按钮样式进行修改

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,33 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Button, Space, Divider } from "antd";
+import {
+  PlusOutlined,
+  BarsOutlined,
+  StarOutlined,
+  DeleteOutlined
+} from "@ant-design/icons";
 import styles from "./ManageLayout.module.scss";
 
 const ManageLayout: FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout Left</p>
-        <button>创建问卷</button> <hr />
-        <button>我的问卷</button>
-        <hr />
-        <button>星标问卷</button>
-        <hr />
-        <button>回收站</button>
-        <hr />
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider style={{ borderTop: "transparent" }} />
+          <Button type="default" size="large" icon={<BarsOutlined />}>
+            我的问卷
+          </Button>
+          <Button type="default" size="large" icon={<StarOutlined />}>
+            星标问卷
+          </Button>
+          <Button type="default" size="large" icon={<DeleteOutlined />}>
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
