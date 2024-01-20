bd8223cb2896219ffaf67542d0b8f5e7af8b0841
LHdensity <1252995152@qq.com>
2024-01-20 21:36:38 +0800

    设置mainLayout样式：header左右两个浮动实现+main+footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import styles from "./MainLayout.module.scss";
+
+const { Header, Content, Footer } = Layout;
+
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>
+        漫旅问卷 &copy; 2023-present. Created by LHJ
+      </Footer>
+    </Layout>
   );
 };
 
