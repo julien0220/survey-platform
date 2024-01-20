fdad245fd5d80375fd0711909d66e9e16eec52b5
LHdensity <1252995152@qq.com>
2024-01-20 22:00:08 +0800

    添加nav完成跳转。添加location方法获取当前所在页面，让按钮状态随之变化

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { Button, Space, Divider } from "antd";
 import {
   PlusOutlined,
@@ -10,6 +10,8 @@ import {
 import styles from "./ManageLayout.module.scss";
 
 const ManageLayout: FC = () => {
+  const nav = useNavigate();
+  const { pathname } = useLocation();
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -18,13 +20,34 @@ const ManageLayout: FC = () => {
             新建问卷
           </Button>
           <Divider style={{ borderTop: "transparent" }} />
-          <Button type="default" size="large" icon={<BarsOutlined />}>
+          <Button
+            type={pathname.startsWith("/manage/list") ? "default" : "text"}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => {
+              nav("/manage/list");
+            }}
+          >
             我的问卷
           </Button>
-          <Button type="default" size="large" icon={<StarOutlined />}>
+          <Button
+            type={pathname.startsWith("/manage/star") ? "default" : "text"}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => {
+              nav("/manage/star");
+            }}
+          >
             星标问卷
           </Button>
-          <Button type="default" size="large" icon={<DeleteOutlined />}>
+          <Button
+            type={pathname.startsWith("/manage/trash") ? "default" : "text"}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => {
+              nav("/manage/trash");
+            }}
+          >
             回收站
           </Button>
         </Space>
