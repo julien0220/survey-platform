8ae3b5ba22fa4edfd098337278a975642a9b768b
LHdensity <1252995152@qq.com>
2024-01-23 17:00:27 +0800

    登录页基本UI开发（剩校验、请求

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,92 @@
-import React, { FC } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { FC, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Typography, Space, Form, Input, Button, Checkbox } from "antd";
+import { UserAddOutlined } from "@ant-design/icons";
+import { REGISTER_PATHNAME } from "../router";
+import styles from "./Login.module.scss";
+
+const { Title } = Typography;
+
+const USERNAME_KEY = "USERNAME";
+const PADSWORD_KEY = "PASSWORD";
+
+function rememberUser(username: string, password: string) {
+  localStorage.setItem(USERNAME_KEY, username);
+  localStorage.setItem(PADSWORD_KEY, password);
+}
+
+function deleteUserFromStorage() {
+  localStorage.removeItem(USERNAME_KEY);
+  localStorage.removeItem(PADSWORD_KEY);
+}
+
+function getUserFromStorage() {
+  return {
+    username: localStorage.getItem(USERNAME_KEY),
+    password: localStorage.getItem(PADSWORD_KEY)
+  };
+}
 
 const Login: FC = () => {
+  const [form] = Form.useForm();
+  useEffect(() => {
+    const { username, password } = getUserFromStorage();
+    form.setFieldsValue({ username, password });
+  }, []);
+
   const nav = useNavigate();
+  const onFinish = (values: any) => {
+    // any 在适合的情况下写，不要都用
+    console.log(values);
+    const { username, password, remember } = values;
+    if (remember) {
+      rememberUser(username, password);
+    } else {
+      deleteUserFromStorage();
+    }
+  };
 
   return (
-    <div>
-      <p>login</p>
-      <button
-        onClick={() => {
-          nav(-1);
-        }}
-      >
-        返回上一页
-      </button>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserAddOutlined />
+          </Title>
+          <Title level={2}>登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+          initialValues={{ remember: true }}
+          form={form}
+        >
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 6, span: 16 }}
+          >
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>注册新用户</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
