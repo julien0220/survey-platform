dd393c973bdc66cc61664a5a69014e118f8e4d69
LHdensity <1252995152@qq.com>
2024-01-25 23:50:06 +0800

    使用ahooks的useRequest对获取请求数据进行改造

--- src/hooks/useLoadQuestionData.ts
+++ src/hooks/useLoadQuestionData.ts
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useRequest } from "ahooks";
 import { getQuestionService } from "../services/question";
 
 function useLoadQuestionData() {
   const { id = "" } = useParams(); // 解析获取路由动态参数
-  const [loading, setLoading] = useState(true);
-  const [questionData, setQuestionData] = useState({});
 
-  useEffect(() => {
-    async function fn() {
-      const data = await getQuestionService(id);
-      setQuestionData(data);
-      setLoading(false);
-    }
-    fn();
-  }, []);
+  //   const [loading, setLoading] = useState(true);
+  //   const [questionData, setQuestionData] = useState({});
 
-  return { loading, questionData };
+  //   useEffect(() => {
+  //     async function fn() {
+  //       const data = await getQuestionService(id);
+  //       setQuestionData(data);
+  //       setLoading(false);
+  //     }
+  //     fn();
+  //   }, []);
+
+  //   return { loading, questionData };
+
+  async function load() {
+    const data = await getQuestionService(id);
+    return data;
+  }
+  const { loading, data, error } = useRequest(load);
+
+  return { loading, data, error };
 }
 
 export default useLoadQuestionData;
--- src/pages/question/Statistics/index.tsx
+++ src/pages/question/Statistics/index.tsx
@@ -2,12 +2,12 @@ import React, { FC } from "react";
 import useLoadQuestionData from "../../../hooks/useLoadQuestionData";
 
 const Statistics: FC = () => {
-  const { loading, questionData } = useLoadQuestionData();
+  const { loading, data } = useLoadQuestionData();
 
   return (
     <div>
       <p>statistics page</p>
-      {loading ? <p>loading...</p> : <p>{JSON.stringify(questionData)}</p>}
+      {loading ? <p>loading...</p> : <p>{JSON.stringify(data)}</p>}
     </div>
   );
 };
