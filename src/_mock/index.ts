fd29c341831ca6c4f2f50ceb2363d08873baae3d
LHdensity <1252995152@qq.com>
2024-01-25 23:18:19 +0800

    关闭mockJS的简单使用

---
+++ src/_mock/index.ts
@@ -0,0 +1,13 @@
+// import Mock from "mockjs";
+import "./index";
+
+export default null;
+
+// Mock.mock("/api/test", "get", () => {
+//   return {
+//     errno: 0,
+//     data: {
+//       name: `lhj ${Date.now()}`
+//     }
+//   };
+// });
