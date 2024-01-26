352f857717852f11c867b1504ae372c4a24058f6
LHdensity <1252995152@qq.com>
2024-01-26 18:01:53 +0800

    添加滚动触发加载(防抖,useRequest)

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import QuestionCard from "../../components/QuestionCard";
 import { Typography, Spin } from "antd";
 import ListSearch from "../../components/ListSearch";
 import styles from "./Common.module.scss";
-import { useTitle, useDebounceFn } from "ahooks";
+import { useTitle, useDebounceFn, useRequest } from "ahooks";
+import { getQuestionListService } from "../../services/question";
+import { LISt_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from "../../constant";
 
 const { Title } = Typography;
 
@@ -18,15 +20,48 @@ const List: FC = () => {
 
   const [searchParams] = useSearchParams(); // url 参数,没有 page 和 pageSize 参数,但是有 keyword
 
+  // 真正加载
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LISt_PAGE_SIZE,
+        keyword: searchParams.get(LIST_SEARCH_PARAM_KEY) || ""
+      });
+      return data;
+    },
+    {
+      manual: true,
+      onSuccess(result) {
+        const { list: l = [], total = 0 } = result;
+        setList(list.concat(l));
+        setTotal(total);
+        setPage(page + 1);
+      }
+    }
+  );
+
   // 页面初始化、url参数(keyword)变化时
   useEffect(() => {
     tryLoadMore();
   }, [searchParams]);
 
-  // 考虑防抖
+  // 考虑防抖,尝试加载更多
+  const containerRef = useRef<HTMLDivElement>(null);
   const { run: tryLoadMore } = useDebounceFn(
     () => {
       // 判断最底下的元素露出了,才加载更多
+      const elem = containerRef.current;
+      if (elem === null) return;
+
+      const domRect = elem.getBoundingClientRect();
+      if (domRect === null) return;
+
+      const { bottom } = domRect;
+      if (bottom <= document.documentElement.clientHeight) {
+        // 加载更多
+        load();
+      }
     },
     { wait: 1000 }
   );
@@ -40,26 +75,7 @@ const List: FC = () => {
     return () => {
       window.removeEventListener("scroll", tryLoadMore); // 组件卸载时,移除监听(必须做)
     };
-  }, [searchParams]);
-
-  // const [loading, setLoading] = useState(false);
-
-  // // const [searchParams] = useSearchParams();
-  // // console.log("keyword", searchParams.get("keyword")); // 在url里面查找参数
-  // const { data = {}, error, loading } = useLoadQuestionListData();
-  // const { list = [], total = 0 } = data;
-  // // const [list, setList] = useState([]);
-  // // const [total, setTotal] = useState(0);
-
-  // // useEffect(() => {
-  // //   async function load() {
-  // //     const data = await getQuestionListService();
-  // //     const { list, total } = data;
-  // //     setList(list);
-  // //     setTotal(total);
-  // //   }
-  // //   load();
-  // // }, []);
+  }, [searchParams, haveModeData]);
 
   return (
     <>
@@ -72,6 +88,7 @@ const List: FC = () => {
           <ListSearch />
         </div>
       </div>
+      {/* <div style={{ height: "2000px" }}></div> */}
       <div className={styles.content}>
         {loading && (
           <div style={{ textAlign: "center" }}>
@@ -85,7 +102,9 @@ const List: FC = () => {
             return <QuestionCard key={_id} {...q} />;
           })}
       </div>
-      <div className={styles.footer}>load more...... 上划加载更多......</div>
+      <div className={styles.footer}>
+        <div ref={containerRef}>load more...... 上划加载更多......</div>
+      </div>
     </>
   );
 };
