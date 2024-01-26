64b808903525b482626945d1a4bce485c3437ed4
LHdensity <1252995152@qq.com>
2024-01-26 17:04:56 +0800

    分页跳转功能实现

--- src/components/ListPage.tsx
+++ src/components/ListPage.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useState } from "react";
 import { Pagination } from "antd";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate, useLocation } from "react-router-dom";
 import {
   LIST_PAGE_PARAM_KEY,
   LIST_PAGE_SIZE_PARAM_KEY,
@@ -26,7 +26,26 @@ const ListPage: FC<PropsType> = (props: PropsType) => {
     setPageSize(pageSize);
   }, [searchParams]);
 
-  return <Pagination current={current} pageSize={pageSize} total={total} />;
+  // 当 page pageSize 改变时,跳转页面 (改变 url 参数)
+  const nav = useNavigate();
+  const { pathname } = useLocation();
+  function handlePageChange(page: number, pageSize: number) {
+    searchParams.set(LIST_PAGE_PARAM_KEY, page.toString());
+    searchParams.set(LIST_PAGE_SIZE_PARAM_KEY, pageSize.toString());
+    nav({
+      pathname,
+      search: searchParams.toString()
+    });
+  }
+
+  return (
+    <Pagination
+      current={current}
+      pageSize={pageSize}
+      total={total}
+      onChange={handlePageChange}
+    />
+  );
 };
 
 export default ListPage;
