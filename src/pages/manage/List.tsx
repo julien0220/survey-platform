68a8c1dcd6adc5db82b5875f13fee7899fc7d858
LHdensity <1252995152@qq.com>
2024-01-26 17:36:40 +0800

    使用ahooks的防抖函数处理滚动触发加载更多

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,33 +1,65 @@
 import React, { FC, useEffect, useState } from "react";
-// import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import QuestionCard from "../../components/QuestionCard";
 import { Typography, Spin } from "antd";
 import ListSearch from "../../components/ListSearch";
 import styles from "./Common.module.scss";
-import useLoadQuestionListData from "../../hooks/useLoadQuestionListData";
-import { useTitle } from "ahooks";
+import { useTitle, useDebounceFn } from "ahooks";
 
 const { Title } = Typography;
 
 const List: FC = () => {
   useTitle("漫旅问卷 - 我的问卷");
 
-  // const [searchParams] = useSearchParams();
-  // console.log("keyword", searchParams.get("keyword")); // 在url里面查找参数
-  const { data = {}, error, loading } = useLoadQuestionListData();
-  const { list = [], total = 0 } = data;
-  // const [list, setList] = useState([]);
-  // const [total, setTotal] = useState(0);
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const haveModeData = total > list.length;
 
-  // useEffect(() => {
-  //   async function load() {
-  //     const data = await getQuestionListService();
-  //     const { list, total } = data;
-  //     setList(list);
-  //     setTotal(total);
-  //   }
-  //   load();
-  // }, []);
+  const [searchParams] = useSearchParams(); // url 参数,没有 page 和 pageSize 参数,但是有 keyword
+
+  // 考虑防抖
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      console.log("tryLoadMore...");
+    },
+    { wait: 1000 }
+  );
+
+  // 页面加载时,url参数(keyword)变化时
+  useEffect(() => {
+    tryLoadMore();
+  }, [searchParams]);
+
+  // 页面滚动时出发加载
+  useEffect(() => {
+    if (haveModeData) {
+      window.addEventListener("scroll", tryLoadMore);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", tryLoadMore); // 组件卸载时,移除监听(必须做)
+    };
+  }, [searchParams]);
+
+  // const [loading, setLoading] = useState(false);
+
+  // // const [searchParams] = useSearchParams();
+  // // console.log("keyword", searchParams.get("keyword")); // 在url里面查找参数
+  // const { data = {}, error, loading } = useLoadQuestionListData();
+  // const { list = [], total = 0 } = data;
+  // // const [list, setList] = useState([]);
+  // // const [total, setTotal] = useState(0);
+
+  // // useEffect(() => {
+  // //   async function load() {
+  // //     const data = await getQuestionListService();
+  // //     const { list, total } = data;
+  // //     setList(list);
+  // //     setTotal(total);
+  // //   }
+  // //   load();
+  // // }, []);
 
   return (
     <>
