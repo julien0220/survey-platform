3ce839a73bcb9dccc1da4bf144b5215c0069445d
LHdensity <1252995152@qq.com>
2024-01-27 22:31:26 +0800

    添加request 拦截: 每次请求带上 token

--- src/services/ajax.ts
+++ src/services/ajax.ts
@@ -1,9 +1,23 @@
 import axios from "axios";
 import { message } from "antd";
+import { getToken } from "../utils/user-token";
+import { error } from "console";
 
 const instance = axios.create({
   timeout: 10 * 1000
 });
+
+// request 拦截: 每次请求带上 token
+instance.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 // response 拦截: 统一处理 errno 和 msg
 instance.interceptors.response.use((res) => {
