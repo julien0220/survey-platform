536f09f1ebc482e16fec34c966a84e3d8441faab
LHdensity <1252995152@qq.com>
2024-01-30 22:06:24 +0800

    添加StateType

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userReducer";
+import { UserStateTypes } from "./userReducer";
+
+export type StateType = {
+  user: UserStateTypes;
+};
 
 export default configureStore({
   reducer: {
