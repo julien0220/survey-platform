4e7d723f73566e33e4d26e09666276e312e18fca
LHdensity <1252995152@qq.com>
2024-02-01 00:55:53 +0800

    编辑页左侧栏：添加左侧栏

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -4,6 +4,7 @@ import styles from "./index.module.scss";
 import EditCanvas from "./EditCanvas";
 import { useDispatch } from "react-redux";
 import { changeSelectedId } from "../../../store/componentsReducer";
+import LeftPanel from "./leftPanel";
 
 const Edit: FC = () => {
   const { loading } = useLoadQuestionData();
@@ -17,7 +18,9 @@ const Edit: FC = () => {
       <div style={{ backgroundColor: "#fff", height: "40px" }}>Header</div>
       <div className={styles["content-wrapper"]}>
         <div className={styles.content}>
-          <div className={styles.left}>left</div>
+          <div className={styles.left}>
+            <LeftPanel />
+          </div>
           <div className={styles.main} onClick={clearSelectedId}>
             <div className={styles["canvas-wrapper"]}>
               {/* <div style={{ height: "900px" }}>画布，测试滚动</div> */}
