c57a8b47185426db7975ec2cb0d46ab691064452
LHdensity <1252995152@qq.com>
2024-01-31 16:29:22 +0800

    添加画布滚动

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -7,12 +7,14 @@ const Edit: FC = () => {
 
   return (
     <div className={styles.container}>
-      <div style={{ backgroundColor: "#fff" }}>Header</div>
+      <div style={{ backgroundColor: "#fff", height: "40px" }}>Header</div>
       <div className={styles["content-wrapper"]}>
         <div className={styles.content}>
           <div className={styles.left}>left</div>
           <div className={styles.main}>
-            <div className={styles["canvas-wrapper"]}></div>
+            <div className={styles["canvas-wrapper"]}>
+              <div style={{ height: "900px" }}>画布，测试滚动</div>
+            </div>
           </div>
           <div className={styles.right}>right</div>
         </div>
