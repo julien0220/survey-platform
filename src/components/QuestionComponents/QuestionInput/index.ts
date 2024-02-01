f50b4b2466600cf8438fc9f7847b540a2b269c40
LHdensity <1252995152@qq.com>
2024-02-01 10:59:12 +0800

    编辑页左侧栏：定义属性表单，用于修改组件的属性

--- src/components/QuestionComponents/QuestionInput/index.ts
+++ src/components/QuestionComponents/QuestionInput/index.ts
@@ -1,11 +1,13 @@
 import Component from "./Component";
 import { QuestionInputDefaultProps } from "./interface";
+import Propcomponent from "./PropComponent";
 
 export * from "./interface";
 
 export default {
   title: "输入框",
   type: "questionInput", // 要和后端统一好
+  Propcomponent,
   Component,
   defaultProps: QuestionInputDefaultProps
 };
--- src/components/QuestionComponents/QuestionTitle/index.ts
+++ src/components/QuestionComponents/QuestionTitle/index.ts
@@ -1,11 +1,13 @@
 import Component from "./Component";
 import { QuestionTitleDefaultProps } from "./interface";
+import Propcomponent from "./PropComponent";
 
 export * from "./interface";
 
 export default {
   title: "标题",
   type: "questionTitle", // 要和后端统一好
+  Propcomponent,
   Component,
   defaultProps: QuestionTitleDefaultProps
 };
