9162bb8a4e82ccd7c207b494e814dafd2513f394
LHdensity <1252995152@qq.com>
2024-02-05 01:04:12 +0800

    编辑页画布：增加拖拉拽排序功能

--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -6,10 +6,13 @@ import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
 import { getComponentConfByType } from "../../../components/QuestionComponents";
 import {
   ComponentInfoType,
-  changeSelectedId
+  changeSelectedId,
+  moveComponent
 } from "../../../store/componentsReducer";
 import useBindCanvasKeyPress from "../../../hooks/useBindCanvasKeyPress";
 import styles from "./EditCanvas.module.scss";
+import SortableContainer from "../../../components/DragSortable/SortableContainer";
+import SortableItem from "../../../components/DragSortable/SortableItem";
 
 type PropsType = {
   loading: boolean;
@@ -27,11 +30,16 @@ function genComponent(componentInfo: ComponentInfoType) {
 
 const EditCanvas: FC<PropsType> = ({ loading }) => {
   const { componentList, selectedId } = useGetComponentInfo();
+  const componentListWithId = componentList.map((c) => ({ ...c, id: c.fe_id }));
   const dispatch = useDispatch();
 
   function handleClick(event: MouseEvent, id: string) {
     event.stopPropagation();
     dispatch(changeSelectedId(id));
+  }
+
+  function handleDragEnd(oldIndex: number, newIndex: number) {
+    dispatch(moveComponent({ oldIndex, newIndex }));
   }
 
   useBindCanvasKeyPress();
@@ -43,35 +51,39 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
       </div>
     );
   return (
-    <div className={styles.canvas}>
-      {componentList
-        .filter((c) => !c.isHidden)
-        .map((c) => {
-          const { fe_id, isLocked } = c;
+    <SortableContainer items={componentListWithId} onDragEnd={handleDragEnd}>
+      <div className={styles.canvas}>
+        {componentList
+          .filter((c) => !c.isHidden)
+          .map((c) => {
+            const { fe_id, isLocked } = c;
 
-          // 拼接 class name
-          const wrapperDefaultClassName = styles["component-wrapper"];
-          const selectedClassName = styles.selected;
-          const lockedClassName = styles.locked;
-          const wrapperClassName = classNames({
-            [wrapperDefaultClassName]: true,
-            [selectedClassName]: fe_id === selectedId,
-            [lockedClassName]: isLocked
-          });
+            // 拼接 class name
+            const wrapperDefaultClassName = styles["component-wrapper"];
+            const selectedClassName = styles.selected;
+            const lockedClassName = styles.locked;
+            const wrapperClassName = classNames({
+              [wrapperDefaultClassName]: true,
+              [selectedClassName]: fe_id === selectedId,
+              [lockedClassName]: isLocked
+            });
 
-          return (
-            <div
-              key={fe_id}
-              className={wrapperClassName}
-              onClick={(e) => {
-                handleClick(e, fe_id);
-              }}
-            >
-              <div className={styles.component}>{genComponent(c)}</div>
-            </div>
-          );
-        })}
-    </div>
+            return (
+              <SortableItem key={fe_id} id={fe_id}>
+                <div
+                  key={fe_id}
+                  className={wrapperClassName}
+                  onClick={(e) => {
+                    handleClick(e, fe_id);
+                  }}
+                >
+                  <div className={styles.component}>{genComponent(c)}</div>
+                </div>
+              </SortableItem>
+            );
+          })}
+      </div>
+    </SortableContainer>
   );
 };
 
