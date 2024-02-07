a08a54b0990147767194a1ef158215cf59f028ac
LHdensity <1252995152@qq.com>
2024-02-07 23:49:43 +0800

    添加常量：颜色列表

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,3 +4,13 @@ export const LIST_SEARCH_PARAM_KEY = "keyword";
 export const LIST_PAGE_PARAM_KEY = "page";
 export const LIST_PAGE_SIZE_PARAM_KEY = "pageSize";
 export const LISt_PAGE_SIZE = 10;
+export const STAT_COLORS = [
+  "#FF6F61", // 红色
+  "#4CAF50", // 绿色
+  "#2196F3", // 蓝色
+  "#FFC107", // 黄色
+  "#9C27B0", // 紫色
+  "#FF5722", // 橙色
+  "#607D8B", // 灰色
+  "#00BCD4" // 青色
+];
